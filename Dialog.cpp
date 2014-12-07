dd9b3b7517130192f746f331ec78f235fa1fed3d
Enzhaev Ivan <8observer8@gmail.com>
2014-12-07 07:58:31 +0300

    First Commit

--- Dialog.cpp
+++ Dialog.cpp
@@ -16,17 +16,18 @@ Dialog::Dialog(QWidget *parent) :
         x[i] = ( i/50.0 - 1 ) * M_PI; // x goes from -Pi to Pi
         y[i] = std::sin( x[i] );
     }
+
     // Create graph and assign data to it:
     ui->plotWidget->addGraph();
-    ui->plotWidget->graph(0)->setData( x, y );
+    ui->plotWidget->graph( 0 )->setData( x, y );
 
     // Give the axes some labels:
     ui->plotWidget->xAxis->setLabel( "x" );
     ui->plotWidget->yAxis->setLabel( "y" );
 
     // Set axes ranges, so we see all data:
-    ui->plotWidget->xAxis->setRange( -1, 1 );
-    ui->plotWidget->yAxis->setRange( 0, 1 );
+    ui->plotWidget->xAxis->setRange( -M_PI, M_PI );
+    ui->plotWidget->yAxis->setRange( -1, 1 );
 }
 
 Dialog::~Dialog()
--- Sin.pro
+++ Sin.pro
@@ -6,15 +6,17 @@
 
 QT       += core gui
 
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
 
 TARGET = Sin
 TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        Dialog.cpp
+        Dialog.cpp \
+    QCustomPlot.cpp
 
-HEADERS  += Dialog.h
+HEADERS  += Dialog.h \
+    QCustomPlot.h
 
 FORMS    += Dialog.ui
